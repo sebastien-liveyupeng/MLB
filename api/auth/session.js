25db397f7f3305949a5b09fad18087d6774f7760
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2026-01-29 18:19:43 +0100

    Feat: enhance session management by adding authorization header support and improving token handling

--- api/auth/session.js
+++ api/auth/session.js
@@ -3,7 +3,7 @@ const { createClient } = require('@supabase/supabase-js');
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
